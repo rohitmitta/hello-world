125f83f31cbd0f90568b5cbe6eda337e08386e5e
Rohit Mitta <rohitmitta.it@gmail.com>
2019-03-23 11:27:43 +0530

    Updates

---
+++ precache-manifest.dd5700feeeffb4a6c9934a4267f32968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-world/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ffb4ea3617005ab7a82a",
+    "url": "/hello-world/static/js/runtime~main.dc31fdf6.js"
+  },
+  {
+    "revision": "bce14f1383b24b13c0bb",
+    "url": "/hello-world/static/js/main.cbab89ed.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/hello-world/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "bce14f1383b24b13c0bb",
+    "url": "/hello-world/static/css/main.c2cf3883.chunk.css"
+  },
+  {
+    "revision": "c12f51e454c4bf31b208c4202ac25c06",
+    "url": "/hello-world/index.html"
+  }
+];
